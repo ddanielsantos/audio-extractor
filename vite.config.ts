8b2b14700982b1024f738fab1814502967a2f9e0
ddanielsantos <contatodanieljob@gmail.com>
2025-03-19 10:52:06 -0300

    fix: wrong base

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import { homepage as base } from "./package.json";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base,
+	base: '/audio-extractor/',
 	optimizeDeps: {
 		exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
 	},
