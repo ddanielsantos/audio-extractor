9bde95461b1737559bb10bc59e805979b0ac82b2
ddanielsantos <contatodanieljob@gmail.com>
2025-03-19 10:43:58 -0300

    fix: vite base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
+import { homepage as base } from "./package.json";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base,
 	optimizeDeps: {
 		exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
 	},
