e3296124507cb994834da2728f371b6900e39e45
Daniel Santos <80872981+ddanielsantos@users.noreply.github.com>
2025-03-20 21:39:57 -0300

    feat: handl codecs other than aac (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -16,61 +16,105 @@ function App() {
 	 *
 	 * @returns {Promise<void>} A promise that resolves when FFmpeg is loaded.
 	 */
-  const loadFFmpeg = async (): Promise<void> => {
-    const coreUrl = "https://unpkg.com/@ffmpeg/core@0.12.6/dist/esm";
-    const ffmpeg = ffmpegRef.current;
-    await ffmpeg.load({
-      coreURL: await toBlobURL(
-        `${coreUrl}/ffmpeg-core.js`,
-        "text/javascript",
-      ),
-      wasmURL: await toBlobURL(
-        `${coreUrl}/ffmpeg-core.wasm`,
-        "application/wasm",
-      ),
-      workerURL: await toBlobURL(
-        `${coreUrl}/ffmpeg-core.worker.js`,
-        "text/javascript",
-      ),
-    });
-  };
+	const loadFFmpeg = async (): Promise<void> => {
+		const coreUrl = "https://unpkg.com/@ffmpeg/core@0.12.6/dist/esm";
+		const ffmpeg = ffmpegRef.current;
+		await ffmpeg.load({
+			coreURL: await toBlobURL(`${coreUrl}/ffmpeg-core.js`, "text/javascript"),
+			wasmURL: await toBlobURL(
+				`${coreUrl}/ffmpeg-core.wasm`,
+				"application/wasm",
+			),
+		});
+	};
+
+	async function getCodec(ffmpeg: FFmpeg, inputFile: string) {
+		let ffmpegLogs = "";
+		ffmpeg.on("log", (ev) => {
+			ffmpegLogs = ffmpegLogs + `${ev.message}\n`;
+		});
+
+		await ffmpeg.exec(["-i", inputFile]);
+		const matches = ffmpegLogs.match(/Audio: (\w+)/);
+		return matches ? matches[1] : "";
+	}
 
 	/**
 	 * Extracts audio from a video file using FFmpeg.
 	 *
 	 * @param {string} videoUrl - The URL of the video file to extract audio from.
-	 * @returns {Promise<Blob>} A promise that resolves to a Blob containing the extracted audio.
+	 * @param {string} ext - The extension of the video file.
+	 * @returns {Promise<Blob | undefined>} A promise that resolves to a Blob containing the extracted audio.
 	 */
-  const extractWithFFmpeg = async (videoUrl: string): Promise<Blob> => {
-    const ffmpeg = ffmpegRef.current;
-    const inputFile = "input.mp4";
-    const outputFile = "output.aac";
-    await ffmpeg.writeFile(inputFile, await fetchFile(videoUrl));
-    await ffmpeg.exec([
-      "-i",
-      inputFile,
-      "-vn",
-      "-acodec",
-      "copy",
-      outputFile,
-    ]);
-    const data = await ffmpeg.readFile(outputFile);
-    return new Blob([data], {type: "audio/aac"});
-  };
+	const extractWithFFmpeg = async (
+		videoUrl: string,
+		ext: string,
+	): Promise<Blob | undefined> => {
+		const ffmpeg = ffmpegRef.current;
+		const inputFile = `input.${ext}`;
+		await ffmpeg.writeFile(inputFile, await fetchFile(videoUrl));
+		const codec = await getCodec(ffmpeg, inputFile);
+
+		if (!codec) {
+			return;
+		}
+
+		// mp3 and aac streams can be copied directly without re-encoding
+		const outputFile = codec === "mp3" ? "output.mp3" : "output.aac";
+		const type = codec === "mp3" ? "audio/mpeg" : "audio/aac";
+		let ffmpegLogs = "";
+		ffmpeg.on("log", ({ message }) => ffmpegLogs += `${message}\n`);
+		if (["aac", "mp3"].includes(codec)) {
+			await ffmpeg.exec([
+				"-i",
+				inputFile,
+				"-map",
+				"0:a",
+				"-c:a",
+				"copy",
+				outputFile,
+			]);
+		} else {
+			await ffmpeg.exec([
+				"-i",
+				inputFile,
+				"-map",
+				"0:a",
+				"-c:a",
+				"aac",
+				"-b:a",
+				"192k",
+				outputFile,
+			]);
+		}
+		console.info(ffmpegLogs);
+
+		const data = await ffmpeg.readFile(outputFile);
+		if (data instanceof Uint8Array) {
+			return new Blob([data.buffer], { type });
+		}
+
+		return;
+	};
 
 	/**
 	 * Extracts audio from a video file using the specified strategy.
 	 *
 	 * @param {ExtractionStrategy} strategy - The strategy to use for extraction ("ffmpeg" or "audio_context").
 	 * @param {string} videoUrl - The URL of the video file to extract audio from.
+	 * @param {string} ext - The extension of the video file.
 	 * @returns {Promise<Blob | undefined>} A promise that resolves to a Blob containing the extracted audio, or undefined if extraction fails.
 	 */
-  const extract = async (strategy: ExtractionStrategy, videoUrl: string): Promise<Blob | undefined> => {
+	const extract = async (
+		strategy: ExtractionStrategy,
+		videoUrl: string,
+		ext: string,
+	): Promise<Blob | undefined> => {
 		if (strategy === "ffmpeg") {
 			if (!loaded) {
 				try {
-          await loadFFmpeg();
-          setLoaded(true);
+					await loadFFmpeg();
+					setLoaded(true);
 				} catch (e) {
 					console.error("Failed to load FFmpeg", e);
 					return;
@@ -78,9 +122,9 @@ function App() {
 			}
 
 			setExtracting(true);
-      try{
-        return await extractWithFFmpeg(videoUrl);
-      } catch (e) {
+			try {
+				return await extractWithFFmpeg(videoUrl, ext);
+			} catch (e) {
 				console.error("Failed to extract audio", e);
 			} finally {
 				setExtracting(false);
@@ -102,14 +146,22 @@ function App() {
 			return;
 		}
 
+		const ext = file.name.split(".").pop();
+		if (!ext) {
+			return;
+		}
+
+		console.log(file.size)
+
 		const videoUrl = URL.createObjectURL(file);
 		console.time(`audio extraction - ${strategy}`);
-		const audioBlob = await extract(strategy, videoUrl);
+		const audioBlob = await extract(strategy, videoUrl, ext);
 		console.timeEnd(`audio extraction - ${strategy}`);
 		if (!audioBlob) {
 			return;
 		}
 
+		console.log(audioBlob.size)
 		setAudioUrl(URL.createObjectURL(audioBlob));
 	};
 
